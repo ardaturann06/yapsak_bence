9540bca173b4368723120c4811e830ef32dfc0f3
Arda Turan Özdemir <byhypnos@Arda-MacBook-Air.local>
2026-03-18 00:23:50 +0300

    fix: mobilde header butonları daha kompakt

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline cache + background notifications
    ============================================================ */
 
-const CACHE_NAME = 'yapsak-bence-v58';
+const CACHE_NAME = 'yapsak-bence-v59';
 const CACHE_FILES = [
   './',
   './index.html',
