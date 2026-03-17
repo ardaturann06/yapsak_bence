c0e558fbc614e8c64649e0e70fd3fe36f2ce8d97
Arda Turan Özdemir <byhypnos@Arda-MacBook-Air.local>
2026-03-18 00:28:41 +0300

    fix: logo biraz sağa kaydırıldı

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline cache + background notifications
    ============================================================ */
 
-const CACHE_NAME = 'yapsak-bence-v59';
+const CACHE_NAME = 'yapsak-bence-v60';
 const CACHE_FILES = [
   './',
   './index.html',
